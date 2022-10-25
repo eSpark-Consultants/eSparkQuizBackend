78f7e3617644b9c2e9b915163a79c501b903fe86
unknown <mraza@esparkconsultants.com>
2022-10-25 15:02:11 +0500

    Item add/delete/edit services

--- src/schema/Item/item.type.js
+++ src/schema/Item/item.type.js
@@ -14,6 +14,35 @@ const Item = gql`
     createdAt: Date
     updatedAt: Date
   }
+
+  type ItemResponse {
+    status: Boolean
+    message: String
+    data: Item
+    error: String
+  }
+
+  type ItemArrayResponse {
+    status: Boolean
+    message: String
+    data: [Item]
+    error: String
+  }
+
+  input where {
+    categoryId: Int
+  }
+
+  type Query {
+    getAllItems(where: where): ItemArrayResponse
+    getItemById(id: Int!): ItemResponse
+  }
+
+  type Mutation {
+    addNewItem(name: String!, image: String!, price: Int!, categoryId: Int!): ItemResponse
+    updateItem(id: Int!, name: String, image: String, price: Int, categoryId: Int): ItemResponse
+    deleteItem(id: Int!): ItemResponse
+  }
 `;
 
 module.exports = {
